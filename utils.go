743c2b586a1483fc5c0a739bb5eac9c519f42bda
xiangjiwu <xiangjiwu@tencent.com>
2019-05-09 11:04:12 +0800

    增加Ip2Long(), IsFile(), IsString()

--- utils.go
+++ utils.go
@@ -1,34 +1,89 @@
 package utils
 
 import (
-    "bytes"
-    "os/exec"
-    "strconv"
+	"bytes"
+	"encoding/binary"
+	"net"
+	"os"
+	"os/exec"
+	"reflect"
+	"strconv"
+	"strings"
 )
 
-
-func StringToInt(str string) (int, error)  {
-    return strconv.Atoi(str)
+// string 转 int
+func StringToInt(str string) (int, error) {
+	return strconv.Atoi(strings.TrimSpace(str))
 }
 
+// string 转 int64
 func StringToInt64(str string) (int64, error) {
-    return strconv.ParseInt(str,10,64)
+	return strconv.ParseInt(str, 10, 64)
 }
 
+// int 转 string
 func IntToString(num int) string {
-    return strconv.Itoa(num)
+	return strconv.Itoa(num)
 }
 
+// int64 转 string
 func Int64ToString(num int64) string {
-    return strconv.FormatInt(num,10)
+	return strconv.FormatInt(num, 10)
 }
 
-func ExecBashShell(s string) (string, error){
+// int to string
+func IntToByte(num int) []byte {
+	var buffer bytes.Buffer
+	err := binary.Write(&buffer, binary.BigEndian, num)
+	if err != nil {
+		return []byte("")
+	}
+	return buffer.Bytes()
+}
 
-    cmd := exec.Command("/bin/bash", "-c", s)
-    var out bytes.Buffer
-    cmd.Stdout = &out
-    err := cmd.Run()
+// 执行 shell 命令
+func ExecBashShell(s string) (string, error) {
 
-    return out.String(), err
+	cmd := exec.Command("/bin/bash", "-c", s)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+
+	return out.String(), err
+}
+
+// ip 转 long / uint32
+func Ip2Long(ipAddr string) uint32 {
+	ip := net.ParseIP(ipAddr)
+	if ip == nil {
+		return 0
+	}
+	return binary.BigEndian.Uint32(ip.To4())
+}
+
+// long / uint32 转 ip
+func Long2Ip(properAddress uint32) string {
+	ipByte := make([]byte, 4)
+	binary.BigEndian.PutUint32(ipByte, properAddress)
+	ip := net.IP(ipByte)
+	return ip.String()
+}
+
+// 判断是否为文件
+func IsFile(filename string) bool {
+	_, err := os.Stat(filename)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
+// 判断是否为string
+func IsString(val interface{}) bool {
+	v := reflect.ValueOf(val)
+	switch v.Kind() {
+	case reflect.String, reflect.Array:
+		return true
+	}
+	return false
 }
